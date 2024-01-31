12308369121c8984b564d395e11c69c1f71abf17
Давид Чернопятов <vectisraw@gmail.com>
2024-02-01 01:53:58 +0200

    Added update.json

---
+++ NebulaAuth/Core/UpdateManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using AutoUpdaterDotNET;
+
+
+
+namespace NebulaAuth.Core;
+
+public static class UpdateManager
+{
+    public static async Task CheckForUpdates()
+    {
+        AutoUpdater.Start("https://rbsoft.org/updates/AutoUpdaterTest.xml");
+
+    }
+}
