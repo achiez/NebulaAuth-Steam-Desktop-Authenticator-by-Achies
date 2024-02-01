fc143ad1714f37093bfe115ad4a90de3e5528f57
Давид Чернопятов <vectisraw@gmail.com>
2024-02-01 03:06:51 +0200

    1.4.4 auto-update added

--- NebulaAuth/Core/UpdateManager.cs
+++ NebulaAuth/Core/UpdateManager.cs
@@ -1,16 +1,24 @@
-﻿using System.Threading.Tasks;
-using AutoUpdaterDotNET;
-
+﻿using AutoUpdaterDotNET;
+using NebulaAuth.Model;
+using System;
+using System.IO;
 
 
 namespace NebulaAuth.Core;
 
 public static class UpdateManager
 {
-    private const string UPDATE_URL = "https://raw.githubusercontent.com/achiez/NebulaAuth-Steam-Desktop-Authenticator-by-Achies/master/NebulaAuth/update.json";
+    private const string UPDATE_URL = "https://raw.githubusercontent.com/achiez/NebulaAuth-Steam-Desktop-Authenticator-by-Achies/master/NebulaAuth/update.xml";
     public static void CheckForUpdates()
     {
+        string jsonPath = Path.Combine(Environment.CurrentDirectory, "update-settings.json");
+        AutoUpdater.PersistenceProvider = new JsonFilePersistenceProvider(jsonPath);
+        AutoUpdater.ShowSkipButton = false;
+        if (Settings.Instance.AllowAutoUpdate)
+            AutoUpdater.UpdateMode = Mode.ForcedDownload;
         AutoUpdater.Start(UPDATE_URL);
+       
 
     }
+
 }
